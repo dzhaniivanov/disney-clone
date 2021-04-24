8a111707de16dab70a46a12848d028cb7d118184
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-04-24 09:52:20 +0300

    add base styles

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Login = (props) => {
+    return (
+        <Container>
+            <Content>Content</Content>
+        </Container>
+    )
+};
+
+const Container=styled.section`
+overflow:hidden;
+display:flex
+`;
+
+export default Login;
