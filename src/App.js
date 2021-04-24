490ca1983a3d204af23017f5ebe7a64dda4f06ad
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-04-24 18:16:18 +0300

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Login'
+import Login from './components/Login';
+import Header from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
