01acc7b20582d4e45a49d09a0075796a92a696b7
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-04-30 16:25:46 +0300

    movie state added

--- src/features/movie/movieSlice.js
+++ src/features/movie/movieSlice.js
@@ -17,7 +17,14 @@ const movieSlice = createSlice({
             state.newDisney = action.payload.newDisney
             state.original = action.payload.original
             state.trending = action.payload.trending
+        },
+    },
+});
 
-        }
-    }
-})
+export const { setMovie } = movieSlice.actions;
+export const selectRecommend = state => state.movie.recommend;
+export const selectNewDisney = state => state.movie.newDisney;
+export const selectOriginal = state => state.movie.original;
+export const selectTrending = state => state.movie.trending;
+
+export default movieSlice.reducer;  
